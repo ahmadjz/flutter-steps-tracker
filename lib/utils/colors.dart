6e026dd09387d9eabcb16bf93032a4c9ffc8f9c1
Ahmad jz <ahmadjzz99@gmail.com>
2022-09-19 19:30:21 +0300

    add new colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,4 +4,6 @@ const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
 const blueColor = Color.fromRGBO(0, 149, 246, 1);
 const primaryColor = Colors.white;
 const secondaryColor = Colors.grey;
+const darkGrey = Color.fromARGB(255, 58, 55, 55);
 const cardBackground = Color.fromARGB(255, 86, 40, 40);
+const activeColor = Color.fromARGB(255, 238, 110, 93);
