2c7c8db457c4dff6c8bbbfbb05c001eb024a1265
Ahmad jz <ahmadjzz99@gmail.com>
2022-09-19 18:28:23 +0300

    create MyDatabase class for handling database
    MyDatabase class is responsible for handling logic with firestore database

---
+++ lib/services/my_database.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class MyDatabase with ChangeNotifier {
+  MyDatabase({required this.uid});
+  final String uid;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  int _points = 0;
+
+  int get points {
+    return _points;
+  }
+
+  Future<void> initDatabase() async {
+    final userData = await _firestore.collection("users").doc(uid).get();
+    final mydoc = userData.data();
+    if (mydoc?["points"] != null) {
+      _points = mydoc!["points"];
+    }
+    print(_points);
+  }
+}
