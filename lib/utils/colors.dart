0c43b514d3445154099be97294c0afbca4a71939
Ahmad jz <ahmadjzz99@gmail.com>
2022-09-19 07:29:49 +0300

    create colors constants

---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const blueColor = Color.fromRGBO(0, 149, 246, 1);
+const primaryColor = Colors.white;
+const secondaryColor = Colors.grey;
