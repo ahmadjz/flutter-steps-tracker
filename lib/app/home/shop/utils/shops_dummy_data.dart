936d634fef7e5c021f9eddb26926f3fb83cd3a24
Ahmad jz <ahmadjzz99@gmail.com>
2022-09-19 22:04:57 +0300

    create dummy data for shops

---
+++ lib/app/home/shop/utils/shops_dummy_data.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_steps_tracker/app/home/shop/models/shop.dart';
+
+final List<Shop> shops = [
+  Shop(id: "1", name: "Uber", cost: 10),
+  Shop(id: "2", name: "Starbucks", cost: 15),
+  Shop(id: "3", name: "Burger", cost: 20),
+  Shop(id: "4", name: "Groceries", cost: 25),
+  Shop(id: "5", name: "KFC", cost: 30),
+  Shop(id: "6", name: "Parcking", cost: 35),
+  Shop(id: "7", name: "Netflix", cost: 40),
+  Shop(id: "8", name: "Amazon", cost: 40),
+  Shop(id: "9", name: "Cinema", cost: 40),
+];
