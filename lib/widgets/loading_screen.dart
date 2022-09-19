ecd7f900adf4e634fdbf94ba3232e7be27e33a59
Ahmad jz <ahmadjzz99@gmail.com>
2022-09-19 10:29:38 +0300

    create my own loading widget

---
+++ lib/widgets/loading_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_steps_tracker/utils/colors.dart';
+import 'package:loading_indicator/loading_indicator.dart';
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          color: Colors.transparent,
+          width: 100,
+          height: 100,
+          child: const LoadingIndicator(
+              indicatorType: Indicator.ballSpinFadeLoader,
+              colors: [blueColor],
+              backgroundColor: Colors.transparent,
+              pathBackgroundColor: Colors.black),
+        ),
+        const SizedBox(
+          height: 30,
+        )
+      ],
+    ));
+  }
+}
