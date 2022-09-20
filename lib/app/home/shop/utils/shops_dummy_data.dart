45a24075f8a80db468cc14772e3d616bd8c18827
Ahmad jz <ahmadjzz99@gmail.com>
2022-09-20 12:01:09 +0300

    update costs

--- lib/app/home/shop/utils/shops_dummy_data.dart
+++ lib/app/home/shop/utils/shops_dummy_data.dart
@@ -1,13 +1,13 @@
 import 'package:flutter_steps_tracker/app/home/shop/models/shop.dart';
 
 final List<Shop> shops = [
-  Shop(id: "1", name: "Uber", cost: 10),
-  Shop(id: "2", name: "Starbucks", cost: 15),
-  Shop(id: "3", name: "Burger", cost: 20),
-  Shop(id: "4", name: "Groceries", cost: 25),
-  Shop(id: "5", name: "KFC", cost: 30),
-  Shop(id: "6", name: "Parcking", cost: 35),
-  Shop(id: "7", name: "Netflix", cost: 40),
-  Shop(id: "8", name: "Amazon", cost: 40),
-  Shop(id: "9", name: "Cinema", cost: 40),
+  Shop(id: "1", name: "Uber", cost: 100),
+  Shop(id: "2", name: "Starbucks", cost: 120),
+  Shop(id: "3", name: "Burger", cost: 130),
+  Shop(id: "4", name: "Groceries", cost: 150),
+  Shop(id: "5", name: "KFC", cost: 160),
+  Shop(id: "6", name: "Parcking", cost: 100),
+  Shop(id: "7", name: "Netflix", cost: 100),
+  Shop(id: "8", name: "Amazon", cost: 100),
+  Shop(id: "9", name: "Cinema", cost: 100),
 ];
